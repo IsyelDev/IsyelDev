3683bfe4e27c0e62c58ecf5e4e665ea62bdb94f5
Totti Code <163216274+IsyelDev@users.noreply.github.com>
2025-02-12 22:43:24 -0500

    Create dessa.py

---
+++ dessa.py
@@ -0,0 +1,8 @@
+👨‍💻  I am Currently Working at Technologies in Peru as Backend Developer.
+💡  I like to explore new technologies and develop software solutions and quick hacks.
+🎓  I'm currently studying Computer Science and Information Technology at New Horizon College of Engineering .
+🌱  I'm on track for learning more about Web3, Microservice Based Architecture, Machine Learning.
+✍️  In my free time, I pursue Logo Designing and Blog Writing as hobbies/side hustles.
+💬  Feel free to reach out to me for pro bono consulting and volunteering, or just for some interesting discussion.
+✉️  You can shoot me an email at emdelapazt@gmail.com! I'll try to respond as soon as I can.
+📄  Please have a look at my Résumé for more details about me. I'm open to feedback and suggestions!
